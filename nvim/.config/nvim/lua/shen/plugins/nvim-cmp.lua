2bf3ab81fd6db83ec5dfdb7a63461218ead7af90
shen <themhsalimi@gmail.com>
2022-11-08 02:06:48 +0330

    change nvim-cmp keymaps

--- nvim/.config/nvim/lua/shen/plugins/nvim-cmp.lua
+++ nvim/.config/nvim/lua/shen/plugins/nvim-cmp.lua
@@ -20,13 +20,13 @@ cmp.setup({
 		end,
 	},
 	mapping = cmp.mapping.preset.insert({
-		["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
-		["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
+		["<S-tab>"] = cmp.mapping.select_prev_item(), -- previous suggestion
+		["<tab>"] = cmp.mapping.select_next_item(), -- next suggestion
 		["<C-b>"] = cmp.mapping.scroll_docs(-4),
 		["<C-f>"] = cmp.mapping.scroll_docs(4),
 		["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
 		["<C-e>"] = cmp.mapping.abort(), -- close completion window
-		["<tab>"] = cmp.mapping.confirm({ select = false }),
+		["<CR>"] = cmp.mapping.confirm({ select = false }),
 	}),
 	-- sources for autocompletion
 	sources = cmp.config.sources({

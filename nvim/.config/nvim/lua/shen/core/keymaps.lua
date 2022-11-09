756e20e6728241397d9dcbdadb1da4ffdd18aebc
shen <themhsalimi@gmail.com>
2022-11-09 10:48:03 +0330

    add neogit keymap

--- nvim/.config/nvim/lua/shen/core/keymaps.lua
+++ nvim/.config/nvim/lua/shen/core/keymaps.lua
@@ -1,5 +1,5 @@
 local opts = { noremap = true, silent = true }
-local keymap = vim.keymap -- for conciseness 
+local keymap = vim.keymap -- for conciseness
 
 -- set leader key to space
 vim.g.mapleader = " "
@@ -11,18 +11,17 @@ vim.g.mapleader = " "
 keymap.set("i", "jk", "<ESC>", opts) -- use jk to exit insert mode
 keymap.set("i", "kj", "<ESC>", opts) -- use kj to exit insert mode
 
-
 -----------------------
 -- normal mode keymaps
 -----------------------
-keymap.set("n","<leader>w","<cmd>w!<CR>", opts) -- save current file
-keymap.set("n","<leader>q","<cmd>q<CR>", opts) -- quit
-keymap.set("n","x",'"_x') -- delete single character without copying into register
-keymap.set("n","<leader>h","<cmd>nohl<CR>", opts) -- clear highlight 
-keymap.set("n","<leader>l","<cmd>Mason<CR>",opts) -- toggle Mason
+keymap.set("n", "<leader>w", "<cmd>w!<CR>", opts) -- save current file
+keymap.set("n", "<leader>q", "<cmd>q<CR>", opts) -- quit
+keymap.set("n", "x", '"_x') -- delete single character without copying into register
+keymap.set("n", "<leader>h", "<cmd>nohl<CR>", opts) -- clear highlight
+keymap.set("n", "<leader>l", "<cmd>Mason<CR>", opts) -- toggle Mason
 -- window management
-keymap.set("n","<leader>sv","<C-w>v", opts) -- split window vertically
-keymap.set("n","<leader>sh","<C-w>s", opts) -- split window horizontally
+keymap.set("n", "<leader>sv", "<C-w>v", opts) -- split window vertically
+keymap.set("n", "<leader>sh", "<C-w>s", opts) -- split window horizontally
 keymap.set("n", "<leader>se", "<C-w>=", opts) -- make split windows equal width & height
 keymap.set("n", "<leader>sc", "<cmd>close<CR>", opts) -- close current split window
 
@@ -43,7 +42,7 @@ keymap.set("n", "<s-h>", "<cmd>BufferLineCyclePrev<CR>", opts)
 keymap.set("n", "<s-l>", "<cmd>BufferLineCycleNext<CR>", opts)
 -- file explorer
 keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", opts)
--- vim maximizer 
+-- vim maximizer
 keymap.set("n", "<leader>sm", "<cmd>MaximizerToggle<CR>", opts) -- maximize current split window
 -- telescope
 keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>") -- find files within current working directory, respects .gitignore
@@ -53,3 +52,5 @@ keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>") -- list open buffers
 keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>") -- list available help tags
 -- telescope file browser
 keymap.set("n", "<leader>fe", "<cmd>Telescope file_browser<cr>")
+-- neogit
+keymap.set("n", "<leader>g", "<cmd>Neogit<cr>") -- toggle neogit

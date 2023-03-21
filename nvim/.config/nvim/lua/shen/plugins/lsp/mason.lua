93d3b79ebfb31e0adef6da51e1b13ac639604df4
MohammadHossein <shen@machine.home>
2023-03-21 12:58:01 +0330

    update some stuff

--- nvim/.config/nvim/lua/shen/plugins/lsp/mason.lua
+++ nvim/.config/nvim/lua/shen/plugins/lsp/mason.lua
@@ -25,8 +25,7 @@ mason_lspconfig.setup({
 		"tsserver",
 		"html",
 		"cssls",
-		"sumneko_lua",
-        "gopls",
+		"gopls",
 	},
 	-- auto-install configured servers (with lspconfig)
 	automatic_installation = true, -- not the same as ensure_installed
--- nvim/.config/nvim/lua/shen/plugins/transparent.lua
+++ nvim/.config/nvim/lua/shen/plugins/transparent.lua
@@ -1,21 +1,19 @@
 local trans_setup, trans = pcall(require, "transparent")
 if not trans_setup then
-      return
+	return
 end
 
 trans.setup({
-  enable = true, -- boolean: enable transparent
-  extra_groups = { -- table/string: additional groups that should be cleared
-    -- In particular, when you set it to 'all', that means all available groups
+	extra_groups = { -- table/string: additional groups that should be cleared
+		-- In particular, when you set it to 'all', that means all available groups
 
-    -- example of akinsho/nvim-bufferline.lua
-    "BufferLineTabClose",
-    "BufferlineBufferSelected",
-    "BufferLineFill",
-    "BufferLineBackground",
-    "BufferLineSeparator",
-    "BufferLineIndicatorSelected",
-  },
-  exclude = {}, -- table: groups you don't want to clear
-
+		-- example of akinsho/nvim-bufferline.lua
+		"BufferLineTabClose",
+		"BufferlineBufferSelected",
+		"BufferLineFill",
+		"BufferLineBackground",
+		"BufferLineSeparator",
+		"BufferLineIndicatorSelected",
+	},
+	exclude_groups = {}, -- table: groups you don't want to clear
 })

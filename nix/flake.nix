8ea23576dd7b153a64ea7b34a960f0b0b2a9fc3f
54L1M <themhsalimi@gmail.com>
2024-12-12 22:26:48 +0100

    updated

--- nix/flake.nix
+++ nix/flake.nix
@@ -28,9 +28,7 @@
           go
           gopls
           # Adding python with packages directly
-          (python313.withPackages (ps: with ps; [
-            numpy
-          ])) 
+          python313   
           bat
           vscode
           ripgrep
@@ -48,7 +46,8 @@
           enable = true;
           brews = [
             "mas"
-            "docker"
+            "hugo"
+            "yq"
           ];
           casks = [
             "firefox"
@@ -61,10 +60,12 @@
           masApps = {
           };
           onActivation.cleanup = "zap";
+          onActivation.autoUpdate = true;
+          onActivation.upgrade = true;
 
         };
         fonts.packages = [
-          (pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
+          pkgs.nerd-fonts.jetbrains-mono
         ];
           
         system.activationScripts.applications.text =
@@ -81,7 +82,7 @@
             rm -rf /Applications/Nix\ Apps
             mkdir -p /Applications/Nix\ Apps
             find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
-            while read src; do
+            while read -r src; do
               app_name=$(basename "$src")
               echo "copying $src" >&2
               ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"

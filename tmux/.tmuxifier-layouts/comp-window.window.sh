9f64cee71783c132ef188bcf0f86baee4a182e7a
54L1M <themhsalimi@gmail.com>
2025-01-15 11:39:12 +0100

    Add tmux/.tmuxifier-layouts/comp-window.window.sh

---
+++ tmux/.tmuxifier-layouts/comp-window.window.sh
@@ -0,0 +1,16 @@
+# window 0
+new_window "code"
+run_cmd "workon comp"
+run_cmd "nvim ."
+
+# window 1
+new_window "shell"
+run_cmd "workon comp"
+
+# window 2
+new_window "server"
+run_cmd "workon comp"
+
+# window 3
+new_window "misc"
+run_cmd "workon comp"

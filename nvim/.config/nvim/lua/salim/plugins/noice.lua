4f3304896491e96ab88343e311a9e9916995073b
54L1M <themhsalimi@gmail.com>
2025-01-26 05:44:48 +0100

    Update nvim/.config/nvim/lua/salim/plugins/noice.lua

--- nvim/.config/nvim/lua/salim/plugins/noice.lua
+++ nvim/.config/nvim/lua/salim/plugins/noice.lua
@@ -1,10 +1,10 @@
 return {
-	"folke/noice.nvim",
-	event = "VeryLazy",
-	opts = {
-		messages = { enabled = false },
-	},
-	dependencies = {
-		"MunifTanjim/nui.nvim",
-	},
+	-- "folke/noice.nvim",
+	-- event = "VeryLazy",
+	-- opts = {
+	-- 	messages = { enabled = false },
+	-- },
+	-- dependencies = {
+	-- 	"MunifTanjim/nui.nvim",
+	-- },
 }

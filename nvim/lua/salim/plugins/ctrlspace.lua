668c22c495ab7bfbe0f680f50290b087f82869a6
54L1M <themhsalimi@gmail.com>
2025-04-28 15:18:30 +0200

    Remove nvim/lua/salim/plugins/ctrlspace.lua

--- nvim/lua/salim/plugins/ctrlspace.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"vim-ctrlspace/vim-ctrlspace",
-	lazy = false, -- Load immediately
-	init = function()
-		-- Remap <Leader>fe to open CtrlSpace
-		vim.g.CtrlSpaceDefaultMappingKey = "<Nop>" -- Disable default mappings
-		vim.keymap.set("n", "<Leader>fe", ":CtrlSpace<CR>", { silent = true, noremap = true, desc = "Open CtrlSpace" })
-	end,
-}

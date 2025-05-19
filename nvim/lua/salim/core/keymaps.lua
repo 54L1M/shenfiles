e7f9ae057feb9bece00b2eb6865bc85f7648c1f2
54L1M <themhsalimi@gmail.com>
2025-05-19 11:46:05 +0200

    Update nvim/lua/salim/core/keymaps.lua

--- nvim/lua/salim/core/keymaps.lua
+++ nvim/lua/salim/core/keymaps.lua
@@ -1,35 +1,31 @@
-local opts = { noremap = true, silent = true }
 local keymap = vim.keymap
 -----------------------------
 -- KEYMAPS
 -----------------------------
 
-
 -- set leader key to space
 vim.g.mapleader = " "
 
 -- insert mode keymaps
-keymap.set("i", "jk", "<ESC>", opts) -- use jk to exit insert mode
-keymap.set("i", "kj", "<ESC>", opts) -- use kj to exit insert mode
+keymap.set("i", "jk", "<ESC>", { noremap = true, silent = true, desc = "" }) -- use jk to exit insert mode
+keymap.set("i", "kj", "<ESC>", { noremap = true, silent = true, desc = "" }) -- use kj to exit insert mode
 
 -- normal mode keymaps
-keymap.set("n", "<leader>ww", "<cmd>w!<CR>", opts) -- save current file
-keymap.set("n", "<leader>wa", "<cmd>wa<CR>", opts) -- save all files
-keymap.set("n", "<leader>qq", "<cmd>q<CR>", opts) -- quit
-keymap.set("n", "<leader>qb", "<cmd>bdelete<CR>", opts) -- delete buffer
-keymap.set("n", "<leader>cw", "<cmd>cd%:p:h<CR>", opts) -- change working directory
+keymap.set("n", "<leader>ww", "<cmd>w!<CR>", { noremap = true, silent = true, desc = "Save Current File" }) -- save current file
+keymap.set("n", "<leader>wa", "<cmd>wa<CR>", { noremap = true, silent = true, desc = "Save All Files" }) -- save all files
+keymap.set("n", "<leader>qq", "<cmd>q<CR>", { noremap = true, silent = true, desc = "Quit" }) -- quit
 keymap.set("n", "x", '"_x') -- delete single character without copying into register
-keymap.set("n", "<leader>h", "<cmd>nohl<CR>", opts) -- clear highlight
+keymap.set("n", "<leader>nh", "<cmd>nohl<CR>", { noremap = true, silent = true, desc = "Clear Highlight" }) -- clear highlight
 
-keymap.set("n", "<leader>sv", "<C-w>v", opts) -- split window vertically
-keymap.set("n", "<leader>sh", "<C-w>s", opts) -- split window horizontally
-keymap.set("n", "<leader>se", "<C-w>=", opts) -- make split windows equal width & height
-keymap.set("n", "<leader>sc", "<cmd>close<CR>", opts) -- close current split window
+keymap.set("n", "<leader>sv", "<C-w>v", { noremap = true, silent = true, desc = "Split Window Vertically" }) -- split window vertically
+keymap.set("n", "<leader>sh", "<C-w>s", { noremap = true, silent = true, desc = "Split Window Horizontally" }) -- split window horizontally
+keymap.set("n", "<leader>se", "<C-w>=", { noremap = true, silent = true, desc = "Make Splits Equal" }) -- make split windows equal width & height
+keymap.set("n", "<leader>sc", "<cmd>close<CR>", { noremap = true, silent = true, desc = "Close Current Split Window" }) -- close current split window
 
-keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", opts) -- open new tab
-keymap.set("n", "<leader>tc", "<cmd>tabclose<CR>", opts) -- close tab
-keymap.set("n", "<leader>tn", "<cmd>tabnext<CR>", opts) -- go to next tab
-keymap.set("n", "<leader>tp", "<cmd>tabprevious<CR>", opts) -- go to previous tab
+keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { noremap = true, silent = true, desc = "Open New Tab" }) -- open new tab
+keymap.set("n", "<leader>tc", "<cmd>tabclose<CR>", { noremap = true, silent = true, desc = "Close Tab" }) -- close tab
+keymap.set("n", "<leader>tn", "<cmd>tabnext<CR>", { noremap = true, silent = true, desc = "Next Tab" }) -- go to next tab
+keymap.set("n", "<leader>tp", "<cmd>tabprevious<CR>", { noremap = true, silent = true, desc = "Previous Tab" }) -- go to previous tab
 
 -- Navigate vim panes better
 vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")

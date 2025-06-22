b22d7676b5ac55f1e40c3baf0ac1c4ba9986ab1f
54L1M <themhsalimi@gmail.com>
2025-06-22 15:07:33 +0200

    Update sketchybar/plugins/clock.sh

--- sketchybar/plugins/clock.sh
+++ sketchybar/plugins/clock.sh
@@ -4,4 +4,5 @@
 # Path: ~/.config/sketchybar/plugins/clock.sh
 
 # Update clock with current time (24-hour format)
-sketchybar --set $NAME label="$(date '+%H:%M')"
+sketchybar --set $NAME label="$(date '+%H:%M')" \
+                       label.font="JetBrains Mono:Bold:14.0"

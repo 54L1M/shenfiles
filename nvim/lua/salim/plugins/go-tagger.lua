ea18752120442ed99cbea484f117db2b019ee48f
54L1M <themhsalimi@gmail.com>
2025-08-18 11:06:45 +0200

    Remove nvim/lua/salim/plugins/go-tagger.lua

--- nvim/lua/salim/plugins/go-tagger.lua
+++
@@ -1,8 +0,0 @@
-return {
-	"romus204/go-tagger.nvim",
-	config = function()
-		require("go-tagger").setup({
-			skip_private = true, -- Skip unexported fields (starting with lowercase)
-		})
-	end,
-}

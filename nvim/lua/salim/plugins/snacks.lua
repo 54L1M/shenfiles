2d9d53c19ca32aeb463e2eabff6a6db7ae527efe
54L1M <themhsalimi@gmail.com>
2025-09-09 10:52:20 +0200

    Update nvim/lua/salim/plugins/snacks.lua

--- nvim/lua/salim/plugins/snacks.lua
+++ nvim/lua/salim/plugins/snacks.lua
@@ -78,32 +78,11 @@ return {
 			desc = "Lazygit",
 		},
 		{
-			"<leader>gb",
+			"<leader>gbl",
 			function()
 				Snacks.git.blame_line()
 			end,
 			desc = "Git Blame Line",
-		},
-		{
-			"<leader>gB",
-			function()
-				Snacks.gitbrowse()
-			end,
-			desc = "Git Browse",
-		},
-		{
-			"<leader>gf",
-			function()
-				Snacks.lazygit.log_file()
-			end,
-			desc = "Lazygit Current File History",
-		},
-		{
-			"<leader>gl",
-			function()
-				Snacks.lazygit.log()
-			end,
-			desc = "Lazygit Log (cwd)",
 		},
 		{
 			"<leader>cR",

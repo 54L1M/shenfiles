863540fdaa6fdfb0193f75a1b46ed0ec260e9db7
54L1M <themhsalimi@gmail.com>
2025-10-17 15:13:22 +0200

    Update nvim/lsp/lua_ls.lua

--- nvim/lsp/lua_ls.lua
+++ nvim/lsp/lua_ls.lua
@@ -1,19 +1,15 @@
 return {
-	settings = {
-		Lua = {
-			diagnostics = {
-				globals = { "vim" },
-			},
-			completion = {
-				callSnippet = "Replace",
-			},
-			workspace = {
-				library = {
-					vim.api.nvim_get_runtime_file("", true),
-					[vim.fn.expand("$VIMRUNTIME/lua")] = true,
-					[vim.fn.stdpath("config") .. "/lua"] = true,
-				},
-			},
-		},
-	},
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = { "vim" },
+      },
+      completion = {
+        callSnippet = "Replace",
+      },
+      workspace = {
+        checkThirdParty = false, -- This can also help with performance
+      },
+    },
+  },
 }

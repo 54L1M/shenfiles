a08a2b75497881a54fa74206a49c9da4dcf373b3
54L1M <themhsalimi@gmail.com>
2025-10-17 15:09:35 +0200

    Add scripts/raycast/

---
+++ scripts/raycast/chrome-work-profile.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Chrome Work Profile
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 🤖
+# @raycast.packageName Dev
+
+# Documentation:
+# @raycast.author 54L1M
+
+open -a "Google Chrome" --args --profile-directory="Default"
+

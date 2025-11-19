97834dda5a6d3d332d1bb001f322ad8293dc8cca
54L1M <themhsalimi@gmail.com>
2025-11-20 00:15:59 +0100

    Update nvim/lua/salim/plugins/lualine.lua

--- nvim/lua/salim/plugins/lualine.lua
+++ nvim/lua/salim/plugins/lualine.lua
@@ -17,42 +17,67 @@ return {
 		-- 	inactive_bg = "#181926",
 		-- }
 		-- kanagawa colors
+		-- local colors = {
+		-- 	blue = "#7FB4CA",
+		-- 	green = "#98BB6C",
+		-- 	violet = "#957FB8",
+		-- 	yellow = "#E6C384",
+		-- 	red = "#E46876",
+		-- 	fg = "#938AA9",
+		-- 	bg = "#2A2A37	",
+		-- 	semilightgray = "#54546D",
+		-- 	inactive_bg = "#1F1F28",
+		-- }
+		-- mocha
 		local colors = {
-			blue = "#7FB4CA",
-			green = "#98BB6C",
-			violet = "#957FB8",
-			yellow = "#E6C384",
-			red = "#E46876",
-			fg = "#938AA9",
-			bg = "#2A2A37	",
-			semilightgray = "#54546D",
-			inactive_bg = "#1F1F28",
+			flamingo = "#ea6962",
+			red = "#ea6962",
+			maroon = "#ea6962",
+			mauve = "#d3869b",
+			peach = "#e78a4e",
+			yellow = "#d8a657",
+			green = "#a9b665",
+			sky = "#89b482",
+			blue = "#7daea3",
+			text = "#ebdbb2",
+			subtext1 = "#d5c4a1",
+			subtext0 = "#bdae93",
+			overlay2 = "#a89984",
+			overlay1 = "#928374",
+			overlay0 = "#595959",
+			surface2 = "#4d4d4d",
+			surface1 = "#404040",
+			surface0 = "#292929",
+			base = "#1d2021",
+			mantle = "#191b1c",
+			crust = "#141617",
 		}
 		local my_lualine_theme = {
 			normal = {
-				a = { bg = colors.blue, fg = colors.bg, gui = "bold" },
-				b = { bg = colors.bg, fg = colors.fg },
-				c = { bg = colors.bg, fg = colors.fg },
+				a = { bg = colors.blue, fg = colors.base, gui = "bold" },
+				b = { bg = colors.base, fg = colors.subtext0 },
+				c = { bg = colors.base, fg = colors.subtext0 },
 			},
 			insert = {
-				a = { bg = colors.green, fg = colors.bg, gui = "bold" },
-				b = { bg = colors.bg, fg = colors.fg },
-				c = { bg = colors.bg, fg = colors.fg },
+				a = { bg = colors.green, fg = colors.base, gui = "bold" },
+				b = { bg = colors.base, fg = colors.subtext0 },
+				c = { bg = colors.base, fg = colors.subtext0 },
 			},
+
 			visual = {
-				a = { bg = colors.violet, fg = colors.bg, gui = "bold" },
-				b = { bg = colors.bg, fg = colors.fg },
-				c = { bg = colors.bg, fg = colors.fg },
+				a = { bg = colors.mauve, fg = colors.base, gui = "bold" },
+				b = { bg = colors.base, fg = colors.subtext0 },
+				c = { bg = colors.base, fg = colors.subtext0 },
 			},
 			command = {
-				a = { bg = colors.yellow, fg = colors.bg, gui = "bold" },
-				b = { bg = colors.bg, fg = colors.fg },
-				c = { bg = colors.bg, fg = colors.fg },
+				a = { bg = colors.yellow, fg = colors.base, gui = "bold" },
+				b = { bg = colors.base, fg = colors.subtext0 },
+				c = { bg = colors.base, fg = colors.subtext0 },
 			},
 			replace = {
-				a = { bg = colors.red, fg = colors.bg, gui = "bold" },
-				b = { bg = colors.bg, fg = colors.fg },
-				c = { bg = colors.bg, fg = colors.fg },
+				a = { bg = colors.red, fg = colors.base, gui = "bold" },
+				b = { bg = colors.base, fg = colors.subtext0 },
+				c = { bg = colors.base, fg = colors.subtext0 },
 			},
 			inactive = {
 				a = { bg = colors.inactive_bg, fg = colors.semilightgray, gui = "bold" },
@@ -62,7 +87,6 @@ return {
 		}
 		-- configure lualine with modified theme
 		lualine.setup({
-
 			extensions = { "oil", "trouble", "mason", "quickfix", "ctrlspace" },
 			options = {
 				theme = my_lualine_theme,

916d8cc227b295a453d4bd92aa7eead20d63aa08
54L1M <themhsalimi@gmail.com>
2025-11-21 13:07:34 +0100

    Remove sketchybar/plugins/clock.sh

--- sketchybar/plugins/clock.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Clock Plugin
-# Path: ~/.config/sketchybar/plugins/clock.sh
-
-# Update clock with current time (24-hour format)
-sketchybar --set $NAME label="$(date '+%H:%M')" \
-                       label.font="JetBrains Mono:Bold:14.0"

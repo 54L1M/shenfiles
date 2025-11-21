0976ca099d25a875861c55ee498f23c83fe1b307
54L1M <themhsalimi@gmail.com>
2025-11-21 13:07:35 +0100

    Remove sketchybar/plugins/date.sh

--- sketchybar/plugins/date.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Date Plugin
-# Path: ~/.config/sketchybar/plugins/date.sh
-
-# Update date with current date (abbreviated day, day number, abbreviated month)
-sketchybar --set $NAME label="$(date '+%a %d %b')" \
-                       label.font="JetBrains Mono:Bold:14.0"

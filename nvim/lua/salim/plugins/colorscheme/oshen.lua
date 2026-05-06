f0cf6055220037ae25fd29faaf255a864863cb26
54L1M <themhsalimi@gmail.com>
2026-05-06 12:09:57 +0200

    chore(dotfiles): sync nvim/lua/salim/plugins/colorscheme/oshen.lua

---
+++ nvim/lua/salim/plugins/colorscheme/oshen.lua
@@ -0,0 +1,11 @@
+return {
+	-- "54L1M/Oshen.nvim",
+	dir = vim.fn.expand("~/Documents/Oshen.nvim"),
+	lazy = false,
+	priority = 1000,
+	config = function()
+		require("oshen").setup({
+			transparent = true, -- set false for opaque background
+		})
+	end,
+}
